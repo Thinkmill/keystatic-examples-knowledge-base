4e2b72ec61213c7cfd3ae1c7e4982f99feaa4b96
Simon Vrachliotis <simon@simonswiss.com>
2023-08-01 17:08:53 +1000

    Dynamic OG code images and metadata

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,5 +1,30 @@
+import type { Metadata } from "next";
+
 import Search from "@/components/search";
 import CategoriesList from "@/components/categories-list";
+
+const title = "Puppy Owners Hub";
+const description = "A knowledge base for happy new puppy owners";
+
+export const metadata: Metadata = {
+  title: title,
+  description: description,
+  openGraph: {
+    title: title,
+    description: description,
+    url: "#",
+    siteName: title,
+    images: [
+      {
+        url: `/og`,
+        width: 1600,
+        height: 1200,
+      },
+    ],
+    locale: "en_AU",
+    type: "website",
+  },
+};
 
 export default async function Homepage() {
   return (
